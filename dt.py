e790b92920d62bbbcf6a1d695d473dc7dd502dc3
M Pearson <lissa.pearson@gmail.com>
2019-09-07 14:59:24 -0500

    Updates

--- dt.py
+++ dt.py
@@ -1,98 +1,171 @@
+# -*- coding: utf-8 -*-
 import dash
-from dash.dependencies import Input, Output
-import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
 import pandas as pd
+import plotly.graph_objs as go
+import numpy as np
 
-df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv')
 
-app = dash.Dash(__name__)
+
+#Link to Econ Model Data
+df = pd.read_csv(
+    './Data/EconModel/results_summary_bycrop.csv')
+#unique values of criteria
+sp = df['p'].unique()
+sc1 = df['c1'].unique()
+sc2 = df['c2'].unique()
+
+#unique values of p per crop
+selected_variable = 'p'
+scassava = df[df['crop']=='cassava'][selected_variable].unique()
+sgroundnuts = df[df['crop']=='groundnuts'][selected_variable].unique()
+smaize = df[df['crop']=='maize'][selected_variable].unique()
+ssesame = df[df['crop']=='sesame'][selected_variable].unique()
+ssorghum = df[df['crop']=='sorghum'][selected_variable].unique()
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table(
+        # Header
+        [html.Tr([html.Th(col) for col in dataframe.columns])] +
+
+        # Body
+        [html.Tr([
+            html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+        ]) for i in range(min(len(dataframe), max_rows))]
+    )
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
-    dash_table.DataTable(
-        id='datatable-interactivity',
-        columns=[
-            {"name": i, "id": i, "deletable": True, "selectable": True} for i in df.columns
-        ],
-        data=df.to_dict('records'),
-        editable=True,
-        filter_action="native",
-        sort_action="native",
-        sort_mode="multi",
-        column_selectable="single",
-        row_selectable="multi",
-        row_deletable=True,
-        selected_columns=[],
-        selected_rows=[],
-        page_action="native",
-        page_current= 0,
-        page_size= 10,
-    ),
-    html.Div(id='datatable-interactivity-container')
-])
+    #Add radio buttons for what to adjust
+    html.Div([
+        dcc.RadioItems(
+            id='r-variable',
+            options=[
+                {'label': 'C2', 'value': 'C2'},
+                {'label': 'C1', 'value': 'C1'},
+                {'label': 'P', 'value': 'P'}
+            ],
+            value='C2'
+        )  
+        ]),
+   
+    # turn this into returned html by callback
+                    html.Div(id='s-variable'),
+                    html.Div('sliders'),
+                    html.Div([
+                            html.Div(['Cassava'],className="three columns"),
+                            html.Div([dcc.Slider(
+                                id='sp-cassava',
+                                min=int(scassava.min()),
+                                max=int(scassava.max()),
+                                marks={int(i):str(i) for i in scassava},
+                                step=None,
+                                value=0,
+                            )],className="nine columns"),
+                    ],className="row"),html.Br(),
+                    html.Div([
+                            html.Div(['Groundnuts'],className="three columns"),
+                            html.Div([dcc.Slider(
+                                id='sp-groundnuts',
+                                min=int(sgroundnuts.min()),
+                                max=int(sgroundnuts.max()),
+                                marks={int(i):str(i) for i in sgroundnuts},
+                                step=None,
+                                value=0,
+                            )],className="nine columns"),
+                    ],className="row"),html.Br(),
+                    html.Div([
+                            html.Div(['Maize'],className="three columns"),
+                            html.Div([dcc.Slider(
+                                id='sp-maize',
+                                min=int(smaize.min()),
+                                max=int(smaize.max()),
+                                marks={int(i):str(i) for i in smaize},
+                                step=None,
+                                value=0,
+                            )],className="nine columns"),
+                    ],className="row"),html.Br(),
+                    html.Div([
+                            html.Div(['Sesame'],className="three columns"),
+                            html.Div([dcc.Slider(
+                                id='sp-sesame',
+                                min=int(ssesame.min()),
+                                max=int(ssesame.max()),
+                                marks={int(i):str(i) for i in ssesame},
+                                step=None,
+                                value=0,
+                            )],className="nine columns"),
+                    ],className="row"),html.Br(),
+                    html.Div([
+                            html.Div(['Sorghum'],className="three columns"),
+                            html.Div([dcc.Slider(
+                                id='sp-sorghum',
+                                min=int(ssorghum.min()),
+                                max=int(ssorghum.max()),
+                                marks={int(i):str(i) for i in ssorghum},
+                                step=None,
+                                value=0,
+                            )],className="nine columns"),
+                    ],className="row"),html.Br(),
+                    html.Div([
+                            html.Div(['Set All Crops to:'],className="six columns"),
+                            html.Div([dcc.Dropdown(
+                                id='dd-p',
+                                options = sorted([{'label': i, 'value': i} for i in sp],key = lambda x: x['label']),
+                                value=0
+                            )],className="six columns"),
+                    ],className="row"),html.Br(),
+
+    ])
 
 @app.callback(
-    Output('datatable-interactivity', 'style_data_conditional'),
-    [Input('datatable-interactivity', 'selected_columns')]
+    Output(component_id='s-variable', component_property='children'),
+    [Input(component_id='r-variable', component_property='value')]
 )
-def update_styles(selected_columns):
-    return [{
-        'if': { 'column_id': i },
-        'background_color': '#D2F3FF'
-    } for i in selected_columns]
+def update_output_div(input_value):
+    return 'Adjust settings for {}'.format(input_value)
 
-@app.callback(
-    Output('datatable-interactivity-container', "children"),
-    [Input('datatable-interactivity', "derived_virtual_data"),
-     Input('datatable-interactivity', "derived_virtual_selected_rows")])
-def update_graphs(rows, derived_virtual_selected_rows):
-    # When the table is first rendered, `derived_virtual_data` and
-    # `derived_virtual_selected_rows` will be `None`. This is due to an
-    # idiosyncracy in Dash (unsupplied properties are always None and Dash
-    # calls the dependent callbacks when the component is first rendered).
-    # So, if `rows` is `None`, then the component was just rendered
-    # and its value will be the same as the component's dataframe.
-    # Instead of setting `None` in here, you could also set
-    # `derived_virtual_data=df.to_rows('dict')` when you initialize
-    # the component.
-    if derived_virtual_selected_rows is None:
-        derived_virtual_selected_rows = []
+@app.callback([Output('sp-cassava', 'value'),
+              Output('sp-groundnuts', 'value'),
+              Output('sp-maize', 'value'),
+              Output('sp-sesame', 'value'),
+              Output('sp-sorghum', 'value')],
+              [Input('dd-p', 'value')])
+def update_all_p(value):
+    return value, value, value, value, value
 
-    dff = df if rows is None else pd.DataFrame(rows)
-
-    colors = ['#7FDBFF' if i in derived_virtual_selected_rows else '#0074D9'
-              for i in range(len(dff))]
-
-    return [
-        dcc.Graph(
-            id=column,
-            figure={
-                "data": [
-                    {
-                        "x": dff["country"],
-                        "y": dff[column],
-                        "type": "bar",
-                        "marker": {"color": colors},
-                    }
-                ],
-                "layout": {
-                    "xaxis": {"automargin": True},
-                    "yaxis": {
-                        "automargin": True,
-                        "title": {"text": column}
-                    },
-                    "height": 250,
-                    "margin": {"t": 10, "l": 10, "r": 10},
-                },
-            },
-        )
-        # check if column exists - user may have deleted it
-        # If `column.deletable=False`, then you don't
-        # need to do this check.
-        for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
-    ]
-
+# @app.callback(Output('time-slider', 'children'), [Input('my-dropdown', 'value')])
+# def change_range(dropdown_value): return slider_options(dropdown_value)
+##@app.callback([
+##    Output('sp-cassava', 'value'),
+##    Output('data-table', 'data'),
+##    Output('data-table', 'columns'),
+##    Output('container', 'style')
+##], [Input('data-dropdown', 'value')])
+##def multi_output(value):
+##    if value is None:
+##        raise PreventUpdate
+##
+##    selected = sample_data[value]
+##    data = selected['data']
+##    columns = [
+##        {'name': k.capitalize(), 'id': k}
+##        for k in data[0].keys()
+##    ]
+##    figure = go.Figure(
+##        data=[
+##            go.Bar(x=[x['score']], text=x['title'], name=x['title'])
+##            for x in data
+##        ]
+##    )
+##
+##    return figure, data, columns, selected['style']
 
 if __name__ == '__main__':
-    app.run_server(debug=True,port=8040)
+    app.run_server(debug=True,port=8080)
