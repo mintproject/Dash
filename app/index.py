d775eb86cc6939a9b9e4967a72bb99768a0b4cfb
Maximiliano Osorio <maxiosorio@gmail.com>
2019-09-09 12:45:12 -0300

    Add: import all the file from the model dir

--- app/index.py
+++ app/index.py
@@ -3,8 +3,8 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app
-from models import economic
-
+from models import *
+import importlib
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
@@ -16,9 +16,13 @@ app.layout = html.Div([
               [Input('url', 'pathname')])
 
 def display_page(pathname):
-    if pathname == '/economic':
-        return economic.layout
-    else:
+    try:
+        print(pathname)
+        model_name = str(pathname).replace('/', '')
+        model = importlib.import_module(".%s" % model_name, 'models')
+        return model.layout
+    except Exception as err:
+        print(err)
         return '404'
 
 if __name__ == '__main__':
