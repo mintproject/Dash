a3fd12472324d585b797579963e6859ea7fce51e
M Pearson <lissa.pearson@gmail.com>
2019-10-10 11:03:02 -0500

    Chart by location

--- viz/models/cycles/__init__.py
+++ viz/models/cycles/__init__.py
@@ -8,19 +8,41 @@ import dash_table
 import pandas as pd
 import numpy as np
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
+## FOR LIVE
 from viz.app import app
+##
 
-# Data Management Section: import and massage
+#styling
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+# Data Management Section: import and massage,
+### LIVE
 dmaize = pd.read_csv('./viz/data/cycles/maize.csv')
 dsorghum = pd.read_csv('./viz/data/cycles/sorghum.csv')
+### END LIVE
+
+## LOCAL
+# dmaize = pd.read_csv('Data/cycles/maize.csv')
+# dsorghum = pd.read_csv('Data/cycles/sorghum.csv')
+## END LOCAL
+
 dall = dmaize.append(dsorghum)
 planting_dates =dall['planting_date'].unique()
 planting_min = int(planting_dates.min())
 planting_max = int(planting_dates.max())
+locations = dall['location'].unique()
+
+### *REMOVE* for live
+###Config elements
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+# app.config.suppress_callback_exceptions = True
+###
 
 # Layout
-layout = html.Div([
+app.layout = html.Div([
     html.Div([
         html.Div([
             html.P('Crop:'),
@@ -28,46 +50,82 @@ layout = html.Div([
                 options=[dict(label=x, value=x) for x in dall['crop'].unique()],
                 value=dall['crop'].unique()[0]
                 ),
-            html.P('Location:'),
-            dcc.Dropdown(id='dd_location',
-                options=[dict(label=x, value=x) for x in dall['location'].unique()],
-                value=dall['location'].unique()[0]
-                ),
-            html.P('Year(s):'),
+        ],className="four columns"),
+        html.Div([
+            html.P('Year:'),
             dcc.Dropdown(id='dd_year',
                 options=[dict(label=x, value=x) for x in dall['year'].unique()],
                 value=dall['year'].unique()[0]
                 #multi=True,
                 #placeholder="Selct years. Shows all if none selected.",
                 ),
-            html.P('Planting date range:'),
-            dcc.Slider(
-                id='s_planting',
-                min = planting_min,
-                max = planting_max,
-                marks={int(i):str(i) for i in planting_dates},
-                step=None,
-                value=planting_min,
-            ),
-        ],className="three columns"),
+        ],className="four columns"),
         html.Div([
-            dcc.Graph(id='cycles_graph'),
-        ],className="nine columns"),
+                    html.P('Planting date range:'),
+                    dcc.Slider(
+                        id='s_planting',
+                        min = planting_min,
+                        max = planting_max,
+                        marks={int(i):str(i) for i in planting_dates},
+                        step=None,
+                        value = planting_min
+                    ),
+        ],className="four columns"),
+        html.Div([],className="three columns"),
     ],className="row"),
+    html.Div([
+            html.Div(id='graph')
+    ],className="row"),
+
 ])
 
 # Callbacks
 @app.callback(
-    Output('cycles_graph', 'figure'),
-    [Input('dd_crop', 'value'),Input('dd_location', 'value'),
-    Input('dd_year', 'value'),Input('s_planting', 'value')])
-def update_figure(crop,location,year,planting):
-    filtered_df = dall[(dall.crop == crop)&(dall.location == location)&
+    Output('graph', 'children'),
+    [Input('dd_crop', 'value'),Input('dd_year', 'value'),Input('s_planting', 'value')])
+def update_figure(crop,year,planting):
+    fig_list = []
+    filtered_df = dall[(dall.crop == crop)&
         (dall['planting_date_fixed']==True)&(dall.year == year)&(dall.planting_date == planting)]
-    filtered_df = filtered_df.sort_values('nitrogen_rate')
-    return px.line(
-        filtered_df,
-        x='nitrogen_rate',
-        y='yield',
-        color='weed_fraction',
-    )
+    filtered_df = filtered_df.sort_values('weed_fraction')
+    n=0
+    for l in locations:
+        n=n+1
+        ldata = filtered_df[filtered_df.location == l].sort_values('nitrogen_rate')
+        graphid = 'graph-' + str(n)
+        fig = px.line(
+            ldata,
+            x='nitrogen_rate',
+            y='yield',
+            color='weed_fraction',
+            # colorscale="Viridis",
+            height = 400,
+        )
+        fig.update_traces(mode='lines+markers')
+        fig.update_layout(
+            title_text=l,
+                legend=go.layout.Legend(
+                        x=.7,
+                        y=0,
+                        traceorder="normal",
+                        font=dict(
+                            family="sans-serif",
+                            size=12,
+                            color="black"
+                        ),
+                        bgcolor="LightSteelBlue",
+                        bordercolor="Black",
+                        borderwidth=2
+                    )
+        )
+        lgraph = html.Div([dcc.Graph(
+                        id=graphid,
+                        figure=fig
+                    )],style={'float':'left','width':'50%'})
+        fig_list.append(lgraph)
+    return fig_list
+
+### REMOVE FOR LIVE
+# if __name__ == '__main__':
+#     app.run_server(debug=True,port=8080)
+###
