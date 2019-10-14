9a5ee31379d340700b4d453c467b417ac323c8e7
Maximiliano Osorio <maxiosorio@gmail.com>
2019-10-14 09:50:16 -0300

    fix: handle query params

--- viz/index.py
+++ viz/index.py
@@ -6,20 +6,31 @@ from viz.app import app
 from viz.models import *
 import importlib
 
+from urllib.parse import urlparse, parse_qs   
+
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content')
 ])
 
 @app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
+              inputs=[
+                  Input('url', component_property='pathname'),
+                  Input('url', component_property='search')
+              ])
 
-def display_page(pathname):
+
+def display_page(pathname, search):
     if pathname:
         try:
-            print(pathname)
             model_name = str(pathname).replace('/', '')
-            model = importlib.import_module(".%s" % model_name, 'viz.models')
+            query = urlparse(search).query
+            query_dict = parse_qs(query)
+            for k, v in query_dict.items():
+                print("key: {}, value: {}".format(k,v))
+            model = importlib.import_module(".{}".format(model_name), 'viz.models')
+            model.load_data(query_dict)
+            model = importlib.import_module(".{}.render".format(model_name), 'viz.models')            
             return model.layout
         except Exception as err:
             print("Error:", err)
