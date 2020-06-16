c5ea0c3f9c8d95c1e2a5b367fbbb47e27e0d52d8
Melissa Pearson <lissa.pearson@gmail.com>
2020-06-16 16:25:23 -0400

    calculate bounding box details geotiff and auto center HAND

--- viz/load_data.py
+++ viz/load_data.py
@@ -9,6 +9,7 @@ from solutioncatalog.rest import ApiException
 from pprint import pprint
 from solutioncatalog.download import  download_files
 from pathlib import Path
+from osgeo import gdal
 import pandas as pd
 
 # GET Data from MINT API usint scenario_id, subgoal_id and THREAD_ID
@@ -30,3 +31,19 @@ def get_MINT_data(scenario_id, subgoal_id, thread_id):
     except ApiException as e:
         print("Exception when calling ResultsApi->results_scenario_id_subgoal_id_thread_id_get: %s\n" % e)
 
+# Geto geospatial extent info from a geotiff
+def get_geotiff_details(geotiff):
+    details ={
+        "center": [0,0],
+        "bounds": [(0,0),(0,0)]
+    }
+    data = gdal.Open(geotiff, gdal.GA_ReadOnly)
+    geoTransform = data.GetGeoTransform()
+    if geoTransform is not None:
+        minx = geoTransform[0]
+        maxy = geoTransform[3]
+        maxx = minx + geoTransform[1] * data.RasterXSize
+        miny = maxy + geoTransform[5] * data.RasterYSize
+        details['center'] = [(miny + maxy)/2, (minx + maxx)/2]
+        details['bounds'] = [(miny, minx), (maxy, maxx)]
+    return details
